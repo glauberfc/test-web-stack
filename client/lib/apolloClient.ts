662787c31244fc68550c0840201acf084108813d
Glauber Castro <gfc714@gmail.com>
2022-06-09 21:54:49 -0300

    Change console type

--- client/lib/apolloClient.ts
+++ client/lib/apolloClient.ts
@@ -19,11 +19,11 @@ let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
+      console.error(
         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
       ),
     )
-  if (networkError) console.log(`[Network error]: ${networkError}`)
+  if (networkError) console.error(`[Network error]: ${networkError}`)
 })
 
 const httpLink = new HttpLink({
@@ -52,15 +52,9 @@ function createApolloClient() {
 export function initializeApollo(initialState = null) {
   const _apolloClient = apolloClient ?? createApolloClient()
 
-  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
-  // gets hydrated here
   if (initialState) {
-    // Get existing cache, loaded during client side data fetching
     const existingCache = _apolloClient.extract()
-
-    // Merge the initialState from getStaticProps/getServerSideProps in the existing cache
     const data = merge(existingCache, initialState, {
-      // combine arrays using object equality (like in sets)
       arrayMerge: (destinationArray, sourceArray) => [
         ...sourceArray,
         ...destinationArray.filter((d) =>
@@ -69,12 +63,9 @@ export function initializeApollo(initialState = null) {
       ],
     })
 
-    // Restore the cache with the merged data
     _apolloClient.cache.restore(data)
   }
-  // For SSG and SSR always create a new Apollo Client
   if (typeof window === 'undefined') return _apolloClient
-  // Create the Apollo Client once in the client
   if (!apolloClient) apolloClient = _apolloClient
 
   return _apolloClient
